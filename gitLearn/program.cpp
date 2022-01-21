60e237c085371367a074f8475d69875e38fb0ab5
unknown <201752008@iiitvadodara.ac.in>
2022-01-21 18:47:48 +0530

    Forth commit
    Signed-off-by: unknown <201752008@iiitvadodara.ac.in>

--- gitLearn/program.cpp
+++ gitLearn/program.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 using namespace std;
 int sum1=0;
+int div=0;
 void sum(int a,int b)
 {
     sum1=a+b;
 }
+void division(int a,int b)
+{
+	div=a/b;
+}
 int main() {
 	sum(2,4);
 	cout<<sum1<<" ";
+	cout<<div<<" ";
 	//commented
 	return 0;
 }
